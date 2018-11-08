9f62d1ebd1f38e804a038a0459b0ef4919add10b
Jordan Ariche <whip@Jordans-MacBook-Air.local>
2018-11-08 12:49:03 -0800

    Added SQL, failing on MVN error

--- src/main/java/SportsApp.java
+++ src/main/java/SportsApp.java
@@ -1,13 +1,15 @@
 import javax.xml.bind.JAXBException;
 import java.io.IOException;
+import java.sql.SQLException;
 public class SportsApp {
 
 
 
-    public static void main (String[] args) throws IOException, JAXBException {
+    public static void main (String[] args) throws IOException, JAXBException, SQLException {
         SportsImpl sports = new SportsImpl();
         String xml = sports.connect();
-        sports.StringToJaxb(xml);
+        ArrayOfGame games = sports.StringToJaxb(xml);
+        sports.pojoToDb(games);
         System.out.println("Done");
     }
 }
--- src/main/java/SportsImpl.java
+++ src/main/java/SportsImpl.java
@@ -8,9 +8,17 @@ import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class SportsImpl {
 
+    private static String dbURL = "jdbc:derby://localhost:1527/sample;user=app;password=app";
+    private static String tableName = "cfb";
+    // jdbc Connection
+    private static Connection conn = null;
 
     public String connect() throws IOException {
 
@@ -23,18 +31,17 @@ public class SportsImpl {
         //Response
         int status = con.getResponseCode();
         BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String content = in.readLine().replace("/><","/>\n<");
+        String content = in.readLine().replace("/><", "/>\n<");
 //        StringBuffer content = new StringBuffer();
 //        while ((inputLine = in.readLine()) != null) {
 //            content.append(inputLine);
 //        }
         in.close();
 
-
         return content.toString();
     }
 
-    public void StringToJaxb(String xml) throws JAXBException {
+    public ArrayOfGame StringToJaxb(String xml) throws JAXBException {
 //        System.out.println("\nNEW LINE");
 //        System.out.println(xml);
         System.out.println(xml);
@@ -45,21 +52,82 @@ public class SportsImpl {
         StringReader reader = new StringReader(xml);
         ArrayOfGame games = (ArrayOfGame) unmarshaller.unmarshal(reader);
 
-        for (Game game:games.getGame()) {
-            System.out.println("WEEK**********: "+game.getWeek());
-            System.out.println("Away Team: "+game.getAwayTeamName());
-            System.out.println("Home Team: "+game.getHomeTeamName());
-            System.out.println("DateTime : "+game.getDateTime());
-            System.out.println("Point Spread: "+game.getPointSpread());
-            System.out.println("Home Team Score: "+game.getHomeTeamScore());
+        for (Game game : games.getGame()) {
+            System.out.println("WEEK**********: " + game.getWeek());
+            System.out.println("Away Team: " + game.getAwayTeamName());
+            System.out.println("Home Team: " + game.getHomeTeamName());
+            System.out.println("DateTime : " + game.getDateTime());
+            System.out.println("Point Spread: " + game.getPointSpread());
+            System.out.println("Home Team Score: " + game.getHomeTeamScore());
 
-            if(game.getPeriods().getPeriod() != null) {
+            Double homeScore = 0.0;
+            Double awayScore = 0.0;
+            if (game.getPeriods().getPeriod() != null) {
                 for (Period period : game.getPeriods().getPeriod()) {
                     System.out.println("Period Home: " + period.getHomeScore());
+                    homeScore += period.getHomeScore();
                     System.out.println("Period Away: " + period.getAwayScore());
+                    awayScore += period.getAwayScore();
                 }
+                game.setAwayTeamScore(String.valueOf(awayScore));
+                game.setHomeTeamScore(String.valueOf(homeScore));
             }
 
+        }
+
+        return games;
+    }
+
+    public void pojoToDb(ArrayOfGame games) throws SQLException {
+        createConnection();
+        try {
+            
+            System.out.println("333333333333");
+            String statement = "";
+            for (Game game : games.getGame()) {
+                System.out.println("44444444444");
+                statement = "INSERT INTO "+tableName+" VALUES("
+                        + "'" + game.getAwayTeamScore() + "',"
+                        + "'" + game.getHomeTeamScore() + "',"
+                        + "'" + game.getOverUnder() + "',"
+                        + "'" + game.getPointSpread() + "',"
+                        + "'" + game.getAwayTeamName() + "',"
+                        + "'" + game.getDateTime() + "',"
+                        + "'" + game.getStatus() + "',"
+                        + "'" + game.getWeek() + "',"
+                        + "'" + game.getHomeTeamMoneyLine() + "',"
+                        + "'" + game.getAwayTeamMoneyLine() + "'"
+                        + ")";
+
+                System.out.println("ST: " + statement);
+                Statement st = conn.createStatement();
+                System.out.println("555555555");
+                int m = st.executeUpdate(statement);
+                System.out.println("666666666666");
+                System.out.println("Updated " + m + " rows");
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        } finally {
+            conn.close();
+        }
+    }
+    
+    private static void createConnection()
+    {
+        try
+        {
+            System.out.println("99999999999999");
+            String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+	Class.forName(driver);
+            System.out.println("888888");
+            //Get a connection
+            conn = DriverManager.getConnection(dbURL); 
+            System.out.println("CONNECTION SUCCESSFUL");
+        }
+        catch (Exception except)
+        {
+            except.printStackTrace();
         }
     }
 }
